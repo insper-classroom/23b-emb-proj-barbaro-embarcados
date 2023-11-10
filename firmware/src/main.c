0015b34a77f38acd8bdd1cf82e142911d0afac9c
Nicolas Yassuda <nicolas.yassuda@smartbrain.com.br>
2023-11-09 21:14:27 -0300

    att code

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -19,11 +19,31 @@
 #define LED_IDX      8
 #define LED_IDX_MASK (1 << LED_IDX)
 
+
 // Botão
-#define BUT_PIO      PIOA
-#define BUT_PIO_ID   ID_PIOA
-#define BUT_IDX      11
-#define BUT_IDX_MASK (1 << BUT_IDX)
+#define BUT2_PIO      PIOC
+#define BUT2_PIO_ID   ID_PIOC
+#define BUT2_IDX      13
+#define BUT2_IDX_MASK (1 << BUT2_IDX)
+// Botão
+#define BUT3_PIO      PIOD
+#define BUT3_PIO_ID   ID_PIOD
+#define BUT3_IDX      11
+#define BUT3_IDX_MASK (1 << BUT3_IDX)
+// Botão
+#define BUT4_PIO      PIOD
+#define BUT4_PIO_ID   ID_PIOD
+#define BUT4_IDX      26
+#define BUT4_IDX_MASK (1 << BUT4_IDX)
+// Botão
+#define BUT5_PIO      PIOA
+#define BUT5_PIO_ID   ID_PIOA
+#define BUT5_IDX      24
+#define BUT5_IDX_MASK (1 << BUT5_IDX)
+//Potenciometro
+#define AFEC_POT AFEC0
+#define AFEC_POT_ID ID_AFEC0
+#define AFEC_POT_CHANNEL 0 // Canal do pino PD30
 
 // usart (bluetooth ou serial)
 // Descomente para enviar dados
@@ -38,18 +58,22 @@
 #define USART_COM USART0
 #define USART_COM_ID ID_USART0
 #endif
-
+typedef struct {
+	uint value;
+} adcData;
 /************************************************************************/
 /* RTOS                                                                 */
 /************************************************************************/
 
 #define TASK_BLUETOOTH_STACK_SIZE            (4096/sizeof(portSTACK_TYPE))
 #define TASK_BLUETOOTH_STACK_PRIORITY        (tskIDLE_PRIORITY)
-
+TimerHandle_t xTimer;
+QueueHandle_t xQueueADC;
 /************************************************************************/
 /* prototypes                                                           */
 /************************************************************************/
-
+static void config_AFEC_pot(Afec *afec, uint32_t afec_id, uint32_t afec_channel,
+afec_callback_t callback);
 extern void vApplicationStackOverflowHook(xTaskHandle *pxTask,
 signed char *pcTaskName);
 extern void vApplicationIdleHook(void);
@@ -102,20 +126,81 @@ extern void vApplicationMallocFailedHook(void) {
 /************************************************************************/
 /* handlers / callbacks                                                 */
 /************************************************************************/
+static void AFEC_pot_callback(void) {
+	adcData adc;
+	adc.value = afec_channel_get_value(AFEC_POT, AFEC_POT_CHANNEL);
+	BaseType_t xHigherPriorityTaskWoken = pdTRUE;
+	xQueueSendFromISR(xQueueADC, &adc, &xHigherPriorityTaskWoken);
+}
 
 /************************************************************************/
 /* funcoes                                                              */
 /************************************************************************/
+static void config_AFEC_pot(Afec *afec, uint32_t afec_id, uint32_t afec_channel,
+                            afec_callback_t callback) {
+  /*************************************
+   * Ativa e configura AFEC
+   *************************************/
+  /* Ativa AFEC - 0 */
+  afec_enable(afec);
 
+  /* struct de configuracao do AFEC */
+  struct afec_config afec_cfg;
+
+  /* Carrega parametros padrao */
+  afec_get_config_defaults(&afec_cfg);
+
+  /* Configura AFEC */
+  afec_init(afec, &afec_cfg);
+
+  /* Configura trigger por software */
+  afec_set_trigger(afec, AFEC_TRIG_SW);
+
+  /*** Configuracao específica do canal AFEC ***/
+  struct afec_ch_config afec_ch_cfg;
+  afec_ch_get_config_defaults(&afec_ch_cfg);
+  afec_ch_cfg.gain = AFEC_GAINVALUE_0;
+  afec_ch_set_config(afec, afec_channel, &afec_ch_cfg);
+
+  /*
+  * Calibracao:
+  * Because the internal ADC offset is 0x200, it should cancel it and shift
+  down to 0.
+  */
+  afec_channel_set_analog_offset(afec, afec_channel, 0x200);
+
+  /***  Configura sensor de temperatura ***/
+  struct afec_temp_sensor_config afec_temp_sensor_cfg;
+
+  afec_temp_sensor_get_config_defaults(&afec_temp_sensor_cfg);
+  afec_temp_sensor_set_config(afec, &afec_temp_sensor_cfg);
+
+  /* configura IRQ */
+  afec_set_callback(afec, afec_channel, callback, 1);
+  NVIC_SetPriority(afec_id, 4);
+  NVIC_EnableIRQ(afec_id);
+}
+void vTimerCallback(TimerHandle_t xTimer) {
+	/* Selecina canal e inicializa conversão */
+	afec_channel_enable(AFEC_POT, AFEC_POT_CHANNEL);
+	afec_start_software_conversion(AFEC_POT);
+}
 void io_init(void) {
 
 	// Ativa PIOs
 	pmc_enable_periph_clk(LED_PIO_ID);
-	pmc_enable_periph_clk(BUT_PIO_ID);
+	pmc_enable_periph_clk(BUT2_PIO_ID);
+	pmc_enable_periph_clk(BUT3_PIO_ID);
+	pmc_enable_periph_clk(BUT4_PIO_ID);
+	pmc_enable_periph_clk(BUT5_PIO_ID);
 
 	// Configura Pinos
 	pio_configure(LED_PIO, PIO_OUTPUT_0, LED_IDX_MASK, PIO_DEFAULT | PIO_DEBOUNCE);
-	pio_configure(BUT_PIO, PIO_INPUT, BUT_IDX_MASK, PIO_PULLUP);
+	pio_configure(BUT2_PIO, PIO_INPUT, BUT2_IDX_MASK, PIO_PULLUP);
+	pio_configure(BUT3_PIO, PIO_INPUT, BUT3_IDX_MASK, PIO_PULLUP);
+	pio_configure(BUT4_PIO, PIO_INPUT, BUT4_IDX_MASK, PIO_PULLUP);
+	pio_configure(BUT5_PIO, PIO_INPUT, BUT5_IDX_MASK, PIO_PULLUP);
+	
 }
 
 static void configure_console(void) {
@@ -201,53 +286,90 @@ int hc05_init(void) {
 	vTaskDelay( 500 / portTICK_PERIOD_MS);
 	usart_send_command(USART_COM, buffer_rx, 1000, "AT", 100);
 	vTaskDelay( 500 / portTICK_PERIOD_MS);
-	usart_send_command(USART_COM, buffer_rx, 1000, "AT+NAMEagoravai", 100);
+	usart_send_command(USART_COM, buffer_rx, 1000, "AT+NAMEbarbaro", 100);
 	vTaskDelay( 500 / portTICK_PERIOD_MS);
 	usart_send_command(USART_COM, buffer_rx, 1000, "AT", 100);
 	vTaskDelay( 500 / portTICK_PERIOD_MS);
-	usart_send_command(USART_COM, buffer_rx, 1000, "AT+PIN0000", 100);
+	usart_send_command(USART_COM, buffer_rx, 1000, "AT+PIN1964", 100);
+	
 }
 
 /************************************************************************/
 /* TASKS                                                                */
 /************************************************************************/
-
 void task_bluetooth(void) {
 	printf("Task Bluetooth started \n");
-	
+
 	printf("Inicializando HC05 \n");
 	config_usart0();
 	hc05_init();
+	config_AFEC_pot(AFEC_POT, AFEC_POT_ID, AFEC_POT_CHANNEL, AFEC_pot_callback);
 
+	xTimer = xTimerCreate(/* Just a text name, not used by the RTOS
+						kernel. */
+						"Timer",
+						/* The timer period in ticks, must be
+						greater than 0. */
+						100,
+						/* The timers will auto-reload themselves
+						when they expire. */
+						pdTRUE,
+						/* The ID is used to store a count of the
+						number of times the timer has expired, which
+						is initialised to 0. */
+						(void *)0,
+						/* Timer callback */
+						vTimerCallback);
+	xTimerStart(xTimer, 0);
 	// configura LEDs e Botões
 	io_init();
 
-	char button1 = '0';
-	char eof = 'X';
-
+    char start_of_packet = '<';
+    char end_of_packet = '>';
+    char separator = ',';
+	adcData adc;
+	float value;
+	char str[128];
 	// Task não deve retornar.
 	while(1) {
+		char message[128];
+		int message_len = 0;
+		if(xQueueReceive(xQueueADC, &adc, portMAX_DELAY) == pdPASS){
+			value = ((float)adc.value/(float)4000);
+		}
 		// atualiza valor do botão
-		if(pio_get(BUT_PIO, PIO_INPUT, BUT_IDX_MASK) == 0) {
-			button1 = '1';
-		} else {
-			button1 = '0';
-		}
+		sprintf(str, "%.2f", value);
+		message[message_len++] = start_of_packet;
+		message[message_len++] = (pio_get(BUT2_PIO, PIO_INPUT, BUT2_IDX_MASK) == 0) ? 'L' : '0';
+		message[message_len++] = separator;
 
-		// envia status botão
-		while(!usart_is_tx_ready(USART_COM)) {
-			vTaskDelay(10 / portTICK_PERIOD_MS);
-		}
-		usart_write(USART_COM, button1);
 		
+        message[message_len++] = (pio_get(BUT3_PIO, PIO_INPUT, BUT3_IDX_MASK) == 0) ? 'D' : '0';
+        message[message_len++] = separator;
+
+		
+        message[message_len++] = (pio_get(BUT4_PIO, PIO_INPUT, BUT4_IDX_MASK) == 0) ? 'U' : '0';
+		message[message_len++] = separator;
+		
+		message[message_len++] = (pio_get(BUT5_PIO, PIO_INPUT, BUT5_IDX_MASK) == 0) ? 'R' : '0';
+		message[message_len++] = separator;
+		message[message_len++] = str[0];
+		message[message_len++] = str[1];
+		message[message_len++] = str[2];
+		message[message_len++] = str[3];
+		message[message_len++] = end_of_packet;
+		message[message_len] = '\0';
+
+		while(!usart_is_tx_ready(USART_COM)) {
+			vTaskDelay(1 / portTICK_PERIOD_MS);
+		}
+		usart_put_string(USART_COM, message);
 		// envia fim de pacote
 		while(!usart_is_tx_ready(USART_COM)) {
-			vTaskDelay(10 / portTICK_PERIOD_MS);
+			vTaskDelay(1 / portTICK_PERIOD_MS);
 		}
-		usart_write(USART_COM, eof);
-
 		// dorme por 500 ms
-		vTaskDelay(500 / portTICK_PERIOD_MS);
+		vTaskDelay(1 / portTICK_PERIOD_MS);
 	}
 }
 
@@ -261,7 +383,9 @@ int main(void) {
 	board_init();
 
 	configure_console();
-
+	xQueueADC = xQueueCreate(100, sizeof(adcData));
+	if (xQueueADC == NULL)
+	printf("falha em criar a queue xQueueADC \n");
 	/* Create task to make led blink */
 	xTaskCreate(task_bluetooth, "BLT", TASK_BLUETOOTH_STACK_SIZE, NULL,	TASK_BLUETOOTH_STACK_PRIORITY, NULL);
 
--- python/youtube_controller.py
+++ python/youtube_controller.py
@@ -3,11 +3,14 @@ import serial
 import argparse
 import time
 import logging
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
 
 class MyControllerMap:
     def __init__(self):
-        self.button = {'A': 'L'} # Fast forward (10 seg) pro Youtube
-
+        self.button = {'R': 'D','L':'A','D':'S','U':'W'} # Fast forward (10 seg) pro Youtube
+    
 class SerialControllerInterface:
     # Protocolo
     # byte 1 -> Botão 1 (estado - Apertado 1 ou não 0)
@@ -18,24 +21,75 @@ class SerialControllerInterface:
         self.mapping = MyControllerMap()
         self.incoming = '0'
         pyautogui.PAUSE = 0  ## remove delay
+    def read_packet(self):
+        packet = ''
+        stop = False
+        while not(stop):
+            char = self.ser.read().decode()
+            if char == '<':  # Início do pacote
+                packet = '<'
+            elif char == '>' and packet:  # Fim do pacote
+                packet += '>'
+                stop = True
+            elif packet:
+                packet += char
+       
+        return packet.strip('<>')
     
     def update(self):
         ## Sync protocol
-        while self.incoming != b'X':
-            self.incoming = self.ser.read()
-            logging.debug("Received INCOMING: {}".format(self.incoming))
+        devices = AudioUtilities.GetSpeakers()
+        interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+        volume = cast(interface, POINTER(IAudioEndpointVolume))
+        currentVolumeLevelScalar = volume.GetMasterVolumeLevelScalar()
+        packet = self.read_packet()
+        buttons = packet.split(',')
+        print(buttons)
+        if(float(buttons[4])<1):
+            volume.SetMasterVolumeLevelScalar(float(buttons[4]), None)
+        else:
+            volume.SetMasterVolumeLevelScalar(0.99, None)
+        if buttons[0]=='0':
+            logging.info("KEYUP LEFT")
+            pyautogui.keyUp(self.mapping.button['L'])
+        else:
+            logging.info("KEYDOWN RIGHT")
+            pyautogui.keyDown(self.mapping.button['L'])
+        if buttons[1]=='0':
+            logging.info("KEYUP DOWN")
+            pyautogui.keyUp(self.mapping.button['D'])
+        else:
+            logging.info("KEYDOWN DOWN")
+            pyautogui.keyDown(self.mapping.button['D'])
+        if buttons[2]=='0':
+            logging.info("KEYUP UP")
+            pyautogui.keyUp(self.mapping.button['U'])
+        else:
+            logging.info("KEYDOWN UP")
+            pyautogui.keyDown(self.mapping.button['U'])
+        if buttons[3]=='0':
+            logging.info("KEYUP RIGHT")
+            pyautogui.keyUp(self.mapping.button['R'])
+        else:
+            logging.info("KEYDOWN RIGHT")
+            pyautogui.keyDown(self.mapping.button['R'])
+        # while self.incoming != b'X':
+        #     self.incoming = self.ser.read()
+        #     logging.debug("Received INCOMING: {}".format(self.incoming))
+        # for button in buttons:
+        #     print(button)
+        # data = self.ser.read()
+        # logging.debug("Received DATA: {}".format(data))
+        # print(data)
+        # if data == b'XL':
+        #     logging.info("KEYDOWN A")
+        #     pyautogui.keyDown(self.mapping.button['B'])
+        # elif data == b'L':
+        #     logging.info("KEYUP A")
+        #     pyautogui.keyUp(self.mapping.button['B'])
 
-        data = self.ser.read()
-        logging.debug("Received DATA: {}".format(data))
-
-        if data == b'1':
-            logging.info("KEYDOWN A")
-            pyautogui.keyDown(self.mapping.button['A'])
-        elif data == b'0':
-            logging.info("KEYUP A")
-            pyautogui.keyUp(self.mapping.button['A'])
-
-        self.incoming = self.ser.read()
+        # self.incoming = self.ser.read()
+        self.ser.write(b'C')
 
 
 class DummyControllerInterface:
@@ -43,9 +97,9 @@ class DummyControllerInterface:
         self.mapping = MyControllerMap()
 
     def update(self):
-        pyautogui.keyDown(self.mapping.button['A'])
+        pyautogui.keyDown(self.mapping.button['B'])
         time.sleep(0.1)
-        pyautogui.keyUp(self.mapping.button['A'])
+        pyautogui.keyUp(self.mapping.button['B'])
         logging.info("[Dummy] Pressed A button")
         time.sleep(1)
 
